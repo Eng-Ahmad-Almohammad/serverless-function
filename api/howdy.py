9270de576df559005c341429633e3161fbaa1294
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-07-04 12:54:27 +0300

    fix: remove self.responses and add self.send_response

--- api/howdy.py
+++ api/howdy.py
@@ -2,12 +2,9 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.responses(200)
+        self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         messgae = 'Howdy'
-
         self.wfile.write(messgae.encode())
-
         return
-    
